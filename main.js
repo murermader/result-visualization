cb43047f70e19863e3e84822dcd91b24c242d7a2
Rafael Biehler <mail@rafaelbiehler.com>
2024-10-11 17:23:13 +0200

    Point from GeoJSON, Line

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ console.log("Hello World!")
 
 const basel_coords = [47.56059246043523, 7.590136499248171]
 const dbis_coords = [47.560191795618316, 7.587282445690428]
+const kolllegienhaus_cords = [47.558548044771115, 7.583476523204751]
+const church = [47.55632731812046, 7.591962500574559]
 
 const dotIcon = L.icon({
     iconUrl: 'shapes/dot.png',
@@ -25,3 +27,21 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const markerDefault = L.marker(dbis_coords, {icon: dotIcon}).addTo(map);
+L.marker(kolllegienhaus_cords, {icon: dotIcon}).addTo(map);
+
+const geoJson = {
+    "type": "Feature",
+    "properties": {
+        "name": "Mittlere Brücke Kirchgebäude",
+        "popupContent": "Hi!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [7.5898043, 47.5600440]
+    }
+};
+L.geoJSON(geoJson).addTo(map);
+L.marker(church).addTo(map);
+
+const line = L.polyline([dbis_coords, kolllegienhaus_cords]).addTo(map);
+
